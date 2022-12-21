e82b3e6b51e068e81960e6c6b0ec9675033aa307
soygagi <101847092+soygagi@users.noreply.github.com>
2022-12-20 19:31:15 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ izquierdaX = 0;
 izquierdaY = 0;
 derechaX = 0;
 derechaY = 0;
+scoreizquierda = 0;
 function preload(){
     cancion = loadSound("music.mp3");
 }
@@ -18,6 +19,8 @@ function setup(){
 function gotPoses(results){
     if(results.lenght>0){
         console.log(results);
+        scoreizquierda = results[0].pose.keypoints[9].score;
+        console.log("scoreizquierda = "+scoreizquierda);
        izquierdaX = results[0].pose.leftWrist.x;
        izquierdaY = results[0].pose.leftWrist.y;
        console.log("izquierda x: "+izquierdax+ "izquierdaY: "+izquierdaY);
@@ -31,7 +34,17 @@ function modelLoaded(){
 }
 function draw(){
     image(video, 0, 0, 500,500);
-}
+    fill("FF0000");
+    stroke("#FF0000");
+    if(scoreizquierda > 0.2){
+    circle(izquierdaX, izquierdaY,20);
+    Numeroizquierday = Number(izquierdaY);
+    remover_decimales = floor(Numeroizquierday);
+    volumen = remover_decimales/500;
+    document.getElementById("volumen").innerHTML = "Volumen: "+volumen;
+    cancion.setVolume(volumen);
+}    
+}   
 function play(){
     cancion.play();
     cancion.setVolume(1);
